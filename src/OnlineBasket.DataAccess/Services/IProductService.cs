c7901abc8176aa899efa1b4555bf579a12010afd
Lucas Guzzo <ext.lucas.guzzo@farfetch.com>
2019-05-22 23:05:27 +0100

    Add ProductService

---
+++ src/OnlineBasket.DataAccess/Services/IProductService.cs
@@ -0,0 +1,20 @@
+﻿namespace OnlineBasket.DataAccess.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using OnlineBasket.Domain.Model;
+
+    public interface IProductService
+    {
+        Task<List<Product>> Get(string name, decimal price, int stock);
+
+        Task<Product> Get(Guid id);
+
+        Task<Guid> Create(Product item);
+
+        Task<bool> Update(Guid id, Product item);
+
+        Task<bool> Delete(Guid id);
+    }
+}
