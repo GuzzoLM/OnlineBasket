d1e7fcf4f64891e1fd70097f5a2e05717057a847
Lucas Guzzo <ext.lucas.guzzo@farfetch.com>
2019-05-22 22:47:50 +0100

    Add Product Collection

---
+++ src/OnlineBasket.DataAccess/DataCollections/Implementations/ProductCollection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnlineBasket.Domain.Model;
+
+namespace OnlineBasket.DataAccess.DataCollections.Implementations
+{
+    public class ProductCollection : GenericCollection<Product>, IProducCollection
+    {
+        public ProductCollection()
+        {
+            var items = new List<Product>
+            {
+                new Product
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Red Shirt",
+                    Price = 79.99M,
+                    Stock = 15
+                },
+                new Product
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Blue Shirt",
+                    Price = 99.99M,
+                    Stock = 3
+                },
+                new Product
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "White Shirt",
+                    Price = 59.99M,
+                    Stock = 0
+                }
+            };
+
+            this.Add(items);
+        }
+    }
+}
