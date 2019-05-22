23e940b241d1102ea7c83ad5ea1cfb3cfc90b61b
Lucas Guzzo <ext.lucas.guzzo@farfetch.com>
2019-05-22 22:02:28 +0100

    Make DOmain entities Id aware

--- src/OnlineBasket.Domain/Access/User.cs
+++ src/OnlineBasket.Domain/Access/User.cs
@@ -1,7 +1,12 @@
 ﻿namespace OnlineBasket.Domain.Access
 {
-    public class User
+    using System;
+    using OnlineBasket.Domain.Interfaces;
+
+    public class User : IIdAware
     {
+        public Guid Id { get; set; }
+
         public string UserName { get; set; }
 
         public string Password { get; set; }
---
+++ src/OnlineBasket.Domain/Interfaces/IIdAware.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineBasket.Domain.Interfaces
+{
+    using System;
+
+    public interface IIdAware
+    {
+        Guid Id { get; set; }
+    }
+}
