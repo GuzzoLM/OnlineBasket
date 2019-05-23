f2e12979e0425f40bd2b1495737f706d05169119
Lucas Guzzo <ext.lucas.guzzo@farfetch.com>
2019-05-23 01:05:17 +0100

    Add type adapter between DTO and Domain models

---
+++ src/OnlineBasket/DTO/TypeAdapters/AdapterExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Model = OnlineBasket.Domain.Model;
+
+namespace OnlineBasket.DTO.TypeAdapters
+{
+    public static class AdapterExtensions
+    {
+        public static Model.Product ToModel(this Product product)
+        {
+            return new Model.Product
+            {
+                Id = product.Id ?? Guid.NewGuid(),
+                Name = product.Name,
+                Price = product.Price,
+                Stock = product.Stock
+            };
+        }
+    }
+}
